5e57e39171156263f76db1f99e1161667eb8c9b1
Matt McCormick <mbmccormick@gmail.com>
2011-10-14 10:51:51 -0300

    Edited db/database.sql via GitHub

--- db/database.sql
+++ db/database.sql
@@ -2,6 +2,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(100) NOT NULL,
   `identity` varchar(1000) DEFAULT NULL,
