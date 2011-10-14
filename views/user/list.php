226bbfc05605c074789c4161bed0bf7304c59297
Matt McCormick <mbmccormick@gmail.com>
2011-10-14 10:46:23 -0300

    Edited views/user/list.php via GitHub

--- views/user/list.php
+++ views/user/list.php
@@ -1,12 +1,12 @@
 <div class="content">
     <div class="navigation">
-        <a href="<?=option('base_uri')?>user">Users</a>
+        <a href="<?=option('base_uri')?>users">Users</a>
     </div>
     <div class="list">
         <?=$body?>
     </div>
     <br />
-    <button type="button" class="button" onclick="location.href='<?=option('base_uri')?>user/add';">
+    <button type="button" class="button" onclick="location.href='<?=option('base_uri')?>users/add';">
         <span>New User</span>
     </button>
 </div>
