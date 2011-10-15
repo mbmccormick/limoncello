ba77d7016e0e5e0a7884cdd7d09089a0548bf730
Matt McCormick <mbmccormick@gmail.com>
2011-10-15 13:14:54 -0400

    Updating reset password character set.

--- controllers/security.php
+++ controllers/security.php
@@ -44,10 +44,10 @@
         $user = mysql_fetch_array($result);
         
         $password = "";
-        for ($i = 0; $i < 8; $i++)
+        for ($i = 0; $i < 12; $i++)
         { 
             $d .= rand(1, 30) % 2; 
-            $password .= $d ? chr(rand(65, 90)) : chr(rand(48, 57)); 
+            $password .= $d ? chr(rand(97, 122)) : chr(rand(48, 57)); 
         }
         
         if (mail($user[email], "Your New " . ApplicationName . " Password", "You recently requested a new password for " . ApplicationName . ". Your new password is " . $password . ".\n\n--\n" . ApplicationName . "", "From: " . ApplicationName . " <" . EmailAddress . ">") == true)
