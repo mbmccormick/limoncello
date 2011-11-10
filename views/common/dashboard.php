c70d9a2c263a3aa12bdbd2365a89b224d333d022
Matt McCormick <mbmccormick@gmail.com>
2011-11-10 14:34:38 -0500

    Update views/common/dashboard.php

--- views/common/dashboard.php
+++ views/common/dashboard.php
@@ -1,3 +1,6 @@
+<div class="page-header">
+    <h1><?=$title?> <small><?=$description?></small></h1>
+</div>
 <div class="row">
     <div class="span8">
         Welcome to <?=ApplicationName?>! If you are seeing this page, then you have successfully signed in to this application.
