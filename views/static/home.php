467771f14ed2a096f39150ddeb33d7b3a29a5f02
Matt McCormick <mbmccormick@gmail.com>
2011-11-10 15:08:16 -0500

    Update views/static/home.php

--- views/static/home.php
+++ views/static/home.php
@@ -1,43 +1,43 @@
 <div class="hero-unit">
     <h1><?=ApplicationName?></h1>
-    <p>Vestibulum id ligula porta felis euismod semper. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+    <p>You don't believe in the Force, do you? In my experience, there is no such thing as luck. In my experience, there is no such thing as luck.</p>
     <p><a class="btn primary large" href="<?=option('base_uri')?>about">Learn more »</a></p>
 </div>
 <div class="row">
     <div class="span7">
-        <h2>Heading</h2>
-        <p>Etiam porta sem malesuada magna mollis euismod. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+        <h2>The Battle for Endor</h2>
+        <p>Obi-Wan is here. The Force is with him. I call it luck. Dantooine. They're on Dantooine. A tremor in the Force. The last time I felt it was in the presence of my old master.</p>
         <p><a class="btn" href="#">View details »</a></p>
     </div>
     <div class="span7">
-        <h2>Heading</h2>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-        <p><a class="btn" href="#">View details »</a></p>
-    </div>
-</div>
-<hr>
-<div class="row">
-    <div class="span7">
-        <h2>Heading</h2>
-        <p>Etiam porta sem malesuada magna mollis euismod. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
-        <p><a class="btn" href="#">View details »</a></p>
-    </div>
-    <div class="span7">
-        <h2>Heading</h2>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+        <h2>The Rebel Force</h2>
+        <p>Don't underestimate the Force. The Force is strong with this one. I have you now. You mean it controls your actions? Leave that to me. Send a distress signal, and inform the Senate that all on board were killed. I suggest you try it again, Luke. This time, let go your conscious self and act on instinct.</p>
         <p><a class="btn" href="#">View details »</a></p>
     </div>
 </div>
 <hr>
 <div class="row">
     <div class="span7">
-        <h2>Heading</h2>
-        <p>Etiam porta sem malesuada magna mollis euismod. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+        <h2>The Republic</h2>
+        <p>A tremor in the Force. The last time I felt it was in the presence of my old master. I suggest you try it again, Luke. This time, let go your conscious self and act on instinct. The Force is strong with this one. I have you now.</p>
         <p><a class="btn" href="#">View details »</a></p>
     </div>
     <div class="span7">
-        <h2>Heading</h2>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+        <h2>Revenge of the Sith</h2>
+        <p>The Force is strong with this one. I have you now. As you wish. The more you tighten your grip, Tarkin, the more star systems will slip through your fingers. You are a part of the Rebel Alliance and a traitor! Take her away! What?! Alderaan? I'm not going to Alderaan. I've got to go home. It's late, I'm in for it as it is.</p>
+        <p><a class="btn" href="#">View details »</a></p>
+    </div>
+</div>
+<hr>
+<div class="row">
+    <div class="span7">
+        <h2>The Phantom Menace</h2>
+        <p>The Force is strong with this one. I have you now. You're all clear, kid. Let's blow this thing and go home! I can't get involved! I've got work to do! It's not that I like the Empire, I hate it, but there's nothing I can do about it right now. It's such a long way from here. Hey, Luke! May the Force be with you.</p>
+        <p><a class="btn" href="#">View details »</a></p>
+    </div>
+    <div class="span7">
+        <h2>A New Hope</h2>
+        <p>You are a part of the Rebel Alliance and a traitor! Take her away! I find your lack of faith disturbing. I suggest you try it again, Luke. This time, let go your conscious self and act on instinct.</p>
         <p><a class="btn" href="#">View details »</a></p>
     </div>
 </div>
