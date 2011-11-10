dd43968af7234dcd597c61c21a66171b1c02e463
Matt McCormick <mbmccormick@gmail.com>
2011-11-10 14:30:25 -0500

    Update views/common/dashboard.php

--- views/common/dashboard.php
+++ views/common/dashboard.php
@@ -1,8 +1,5 @@
-<div class="content">
-    <div class="navigation">
-        <a href="<?=option('base_uri')?>">Dashboard</a>
-    </div>
-    <div class="list">
-        Welcome to <?=ApplicationName?>! If you are seeing this page, then you have successfully logged in to this application.
+<div class="row">
+    <div class="span8">
+        Welcome to <?=ApplicationName?>! If you are seeing this page, then you have successfully signed in to this application.
     </div>
 </div>
