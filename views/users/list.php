0ac98ff1fcfb5895a467eec67d4a7280d570b9b7
Matt McCormick <mbmccormick@gmail.com>
2011-11-10 16:59:00 -0500

    Updating views and stylesheets.

--- views/users/list.php
+++ views/users/list.php
@@ -1,17 +1,23 @@
 <div class="page-header">
     <h1><?=$title?></h1>
 </div>
-<table class="bordered-table">
-	<thead>
-		<tr>
-			<th>Name</th>
-			<th style="width: 100px;">Actions</th>
-		</tr>
-	</thead>
-	<tbody>
-		<?=$body?>
-	</tbody>
-</table>
-<div class="well">
-	<a href="<?=option('base_uri')?>users/add" class="btn large primary">New User</a>
+<div class="row">
+	<div class="span10">
+		<table class="bordered-table">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th style="width: 100px;">Actions</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?=$body?>
+			</tbody>
+		</table>
+		<div class="well">
+			<a href="<?=option('base_uri')?>users/add" class="btn primary">New User</a>
+		</div>
+	</div>
+	<div class="span4">
+	</div>
 </div>
