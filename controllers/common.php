53688e86d511cac4f695df91652f7a5d3d1b5eb4
Matt McCormick <mbmccormick@gmail.com>
2011-11-10 14:44:43 -0500

    Update controllers/common.php

--- controllers/common.php
+++ controllers/common.php
@@ -2,9 +2,17 @@
 
 	function common_dashboard()
 	{
-		set("title", "Dashboard");
-        set("description", "This is the home page of the application.");
-		return html("common/dashboard.php");
+        if ($_SESSION['CurrentUser_ID'] != null)
+        {
+            set("title", "Dashboard");
+            set("description", "This is the home page of the application.");
+            return html("common/dashboard.php");
+        }
+        else
+        {
+            set("title", "Home");
+            return html("static/home.php");
+        }
 	}
 
 ?>
