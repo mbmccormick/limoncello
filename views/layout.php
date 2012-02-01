f8b29e90ee5441f2b1e30efe3f981a93e81b5349
Matt McCormick <mbmccormick@gmail.com>
2012-02-01 12:46:20 -0500

    Upgrading to Bootstrap 2.0.

--- views/layout.php
+++ views/layout.php
@@ -4,17 +4,11 @@
     <meta charset="utf-8" /> 
     <title><?=ApplicationName?> - <?=$title?></title> 
     <link rel="stylesheet" href="<?=option('base_uri')?>public/css/bootstrap.css" />
+	<link rel="stylesheet" href="<?=option('base_uri')?>public/css/bootstrap.responsive.css" />
     <link rel="stylesheet" href="<?=option('base_uri')?>public/css/layout.css" />
     <link rel="shortcut icon" type="image/x-icon" href="<?=option('base_uri')?>public/img/logo.ico">
     <script type="text/javascript" src="<?=option('base_uri')?>public/js/jquery-1.7.min.js"></script>
-    <script type="text/javascript" src="<?=option('base_uri')?>public/js/bootstrap-modal.js"></script>
-    <script type="text/javascript" src="<?=option('base_uri')?>public/js/bootstrap-alerts.js"></script>
-    <script type="text/javascript" src="<?=option('base_uri')?>public/js/bootstrap-twipsy.js"></script>
-    <script type="text/javascript" src="<?=option('base_uri')?>public/js/bootstrap-popover.js"></script>
-    <script type="text/javascript" src="<?=option('base_uri')?>public/js/bootstrap-dropdown.js"></script>
-    <script type="text/javascript" src="<?=option('base_uri')?>public/js/bootstrap-scrollspy.js"></script>
-    <script type="text/javascript" src="<?=option('base_uri')?>public/js/bootstrap-tabs.js"></script>
-    <script type="text/javascript" src="<?=option('base_uri')?>public/js/bootstrap-buttons.js"></script>
+    <script type="text/javascript" src="<?=option('base_uri')?>public/js/bootstrap.js"></script>
     <script type="text/javascript" src="<?=option('base_uri')?>public/js/common.js"></script>
 </head> 
 <body>
