ee10f7ee3c46dec612a4573c3cc4c4be4780c8b6
Matt McCormick <mbmccormick@gmail.com>
2012-02-02 14:02:35 -0500

    Upgrading to Bootstrap 2.0.

--- views/security/reset.php
+++ views/security/reset.php
@@ -1,6 +1,5 @@
 <div class="row">
     <form action="<?=option('base_uri')?>login/reset" method="post" class="form-vertical">
-        <p>To reset your account password, please enter your email address below. A new password will be sent to the email address on file.</p>
         <fieldset>
             <div class="control-group">
                 <label class="control-label" for="email">Email Address</label>
--- views/users/add.php
+++ views/users/add.php
@@ -2,49 +2,50 @@
     <div class="span6">
         <form action="<?=option('base_uri')?>users/add" method="post" class="form-vertical">
             <fieldset>
-                <div class="clearfix">
-                    <label for="username">Username</label>
-                    <div class="input">
-                        <input class="xlarge" id="username" name="username" size="30" type="text">
+                <div class="control-group">
+                    <label class="control-label" for="username">Username</label>
+                    <div class="controls">
+                        <input class="input-xlarge" id="username" name="username" type="text" />
                     </div>
                 </div>
                 <br />
-                <div class="clearfix">
-                    <label for="name">Name</label>
-                    <div class="input">
-                        <input class="xlarge" id="name" name="name" size="30" type="text">
+                <div class="control-group">
+                    <label class="control-label" for="name">Name</label>
+                    <div class="controls">
+                        <input class="input-xlarge" id="name" name="name" type="text" />
                     </div>
                 </div>
-                <div class="clearfix">
-                    <label for="email">Email Address</label>
-                    <div class="input">
-                        <input class="xlarge email" id="email" name="email" size="30" type="text">
-                    </div>
-                </div>
-                <br />
-                <div class="clearfix">
-                    <label for="password">Password</label>
-                    <div class="input">
-                        <input class="xlarge" id="password" name="password" size="30" type="password">
-                    </div>
-                </div>
-                <div class="clearfix">
-                    <label for="passwordconfirm">Confirm Password</label>
-                    <div class="input">
-                        <input class="xlarge" id="passwordconfirm" name="passwordconfirm" size="30" type="password">
+                <div class="control-group">
+                    <label class="control-label" for="email">Email Address</label>
+                    <div class="controls">
+                        <input class="input-xlarge email" id="email" name="email" type="text" />
                     </div>
                 </div>
                 <br />
-                <div class="clearfix">
-                    <div class="input">
-                        <input type="checkbox" name="isadministrator" value="1">
-                        <span>This user is an administrator</span>
+                <div class="control-group">
+                    <label class="control-label" for="password">Password</label>
+                    <div class="controls">
+                        <input class="input-xlarge" id="password" name="password" type="password" />
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label" for="passwordconfirm">Confirm Password</label>
+                    <div class="controls">
+                        <input class="input-xlarge" id="passwordconfirm" name="passwordconfirm" type="password" />
+                    </div>
+                </div>
+                <br />
+                <div class="control-group">
+                    <div class="controls">
+                        <label class="checkbox">
+                            <input type="checkbox" name="isadministrator" value="1" /> This user is an administrator
+                        </label>
                     </div>
                 </div>
             </fieldset>
             <br />
             <div class="actions">
-                <button type="submit" class="btn primary">Add User</button>&nbsp;<button type="reset" class="btn">Cancel</button>
+                <button type="submit" class="btn btn-primary">Add User</button>&nbsp;<button type="reset" class="btn">Cancel</button>
             </div>
         </form>
     </div>
